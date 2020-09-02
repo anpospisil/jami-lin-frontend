302d87de5f8f85caf22e4cd3ca485e108ce05e30
anpospisil <anpospisil89@gmail.com>
2020-09-02 15:37:40 +0200

    added Footer component to App

--- src/App.js
+++ src/App.js
@@ -7,17 +7,15 @@ import Loading from "./components/Loading";
 import MessageBox from "./components/MessageBox";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Article from "./pages/Article";
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
 import { getUserWithStoredToken } from "./store/user/actions";
 import { Jumbotron } from "react-bootstrap";
+import Footer from "./components/Footer";
 
-const Home = () => (
-  <Jumbotron>
-    <h1>Home</h1>
-  </Jumbotron>
-);
 const About = () => (
   <Jumbotron>
     <h1>About Jami</h1>
@@ -58,12 +56,14 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/article" component={Article} />
         <Route path="/services" component={Services} />
         <Route path="/shop" component={Shop} />
         <Route path="/cart" component={Cart} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
       </Switch>
+      <Footer />
     </div>
   );
 }
