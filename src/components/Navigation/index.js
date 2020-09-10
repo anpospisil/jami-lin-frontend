fc70fbfda82162ca8f021e0b962e7dfe5f50a7b8
anpospisil <anpospisil89@gmail.com>
2020-09-10 18:06:22 +0200

    added search bar to navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../store/user/selectors";
 import NavbarItem from "./NavbarItem";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
-import "./style.scss"
+import cart from "../../img/cart.svg";
+import "./style.scss";
 
 export default function Navigation() {
   const token = useSelector(selectToken);
@@ -17,7 +17,10 @@ export default function Navigation() {
   return (
     <Navbar sticky="top" expand="lg">
       <Navbar.Brand as={NavLink} to="/">
-        <h2><span className="logo-first">J</span><span className="rest">ami Lin's Passions</span></h2>
+        <h2>
+          <span className="logo-first">J</span>
+          <span className="rest">ami Lin's Passions</span>
+        </h2>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -25,9 +28,22 @@ export default function Navigation() {
           <NavbarItem path="/" linkText="Home" />
           <NavbarItem path="/about" linkText="About Jami" />
           <NavbarItem path="/services" linkText="Services" />
+
           <NavbarItem path="/shop" linkText="Shop" />
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
           {loginLogoutControls}
-          <NavbarItem path="/cart" linkText="Cart" />
+          <NavbarItem path="/cart" linkText="Cart">
+            <img
+              src={cart}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="cart"
+            />
+          </NavbarItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
